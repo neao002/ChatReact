bdc73fafc44344bad6f9f67b0d14eb35b1401b41
Nicolas Alvarez <neao002@gmail.com>
2021-04-07 15:05:55 +0200

    ending

--- src/App.js
+++ src/App.js
@@ -3,16 +3,27 @@ import { ChatEngine } from "react-chat-engine";
 
 import ChatFeed from "./Components/ChatFeed";
 
+import LoginForm from "./Components/LoginForm";
+
 import "./App.css";
 
+const projectID = "d0e35330-ac73-42f7-9d22-598cf64983f3";
+
 const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
   return (
     <ChatEngine
       height="100vh"
-      projectID="d0e35330-ac73-42f7-9d22-598cf64983f3"
-      userName="nico"
-      userSecret="1"
+      projectID={projectID}
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() =>
+        new Audio(
+          "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+        ).play()
+      }
     />
   );
 };
--- src/Components/MyMessage.js
+++ src/Components/MyMessage.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-const MessageForm = ({ message }) => {
-  if (message?.attachments?.lenght > 0) {
+const MyMessage = ({ message }) => {
+  if (message.attachments && message.attachments.length > 0) {
     return (
       <img
         src={message.attachments[0].file}
         alt="message-attachment"
         className="message-image"
         style={{ float: "right" }}
-      ></img>
+      />
     );
   }
 
@@ -19,13 +19,12 @@ const MessageForm = ({ message }) => {
         float: "right",
         marginRight: "18px",
         color: "white",
-        backgroundColor: "#3b2a50",
+        backgroundColor: "#3B2A50",
       }}
     >
-      {" "}
       {message.text}
     </div>
   );
 };
 
-export default MessageForm;
+export default MyMessage;
